f9777a2c7c1d600fabe17bda4b68b6975fdd75f5
Skyler Hopkins <hopkins@aurora>
2018-07-10 05:19:42 -0700

    Uploading runs (K=0-0.4) from FOI = 0.001 and gamma = 0.05. Crashed on K=0.5.

--- TransmissionFunctionSimulationScript_BroadHostRange.R
+++ TransmissionFunctionSimulationScript_BroadHostRange.R
@@ -3,12 +3,13 @@
 ##5 July 2018
 
 library(deSolve) 
+getwd()
 
 #################################################################################
 ###########################Global Variables#####################################
 nrestarts = 5 ##number of fits of each model to each dataset with different starting parameters
 ndatasets = 100 ##number of sample datasets for each K
-FOI<-0.0005 #0.001, 0.0005, 0.0001
+FOI<-0.001 #0.001, 0.0005, 0.0001
 gamma<-0.05 #0.02, 0.05, 0.1
 ks<-seq(0, 1, 0.1) 
 
@@ -376,7 +377,8 @@ for (k in 1:length(ks)) {
     }
   }
   #output one dataframe per K value to a CSV to save
-  write.csv(compareests, paste("/Users/hopkins/Documents/Transmission Function Literature Review/TransmissionFunctions/compareests","K",ks[k],"FOI",FOI,"gamma",gamma,".csv",sep=""), row.names=F)
+  #write.csv(compareests, paste("/Users/hopkins/Documents/Transmission Function Literature Review/TransmissionFunctions/compareests","K",ks[k],"FOI",FOI,"gamma",gamma,".csv",sep=""), row.names=F)
+  write.csv(compareests, paste("/home/hopkins/TransmissionFunctions/compareests","K",ks[k],"FOI",FOI,"gamma",gamma,".csv",sep=""), row.names=F)
   #gc()
 }
 end_time <- Sys.time()
